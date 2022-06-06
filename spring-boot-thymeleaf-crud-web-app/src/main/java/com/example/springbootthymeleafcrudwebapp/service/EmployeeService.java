e46eef214e14366fc87ced00ff65260879210e37
Mark Schledzewski <mark.schledzewski@gmail.com>
2022-06-06 13:19:14 -0400

    springboot thymeleaf webapp

---
+++ spring-boot-thymeleaf-crud-web-app/src/main/java/com/example/springbootthymeleafcrudwebapp/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.example.springbootthymeleafcrudwebapp.service;
+
+import com.example.springbootthymeleafcrudwebapp.model.Employee;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page< Employee > findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+
+}
